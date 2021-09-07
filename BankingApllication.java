e789afaa093aa129dfcc9b8f2d2f3cb32c547a85
Kawa Noman <76186680+kawanoman@users.noreply.github.com>
2021-09-07 12:53:46 +0200

    Update BankingApllication.java

--- BankingApllication.java
+++ BankingApllication.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
 
+
 @SpringBootApplication
 public class BankappApplication {
 
